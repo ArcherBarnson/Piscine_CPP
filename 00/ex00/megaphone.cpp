cf3b516c6ea1f62aef4a85aa51fbb835023113bb
Benjamin GRULOIS <bgrulois@e3r2p23.clusters.42paris.fr>
2023-04-03 11:21:05 +0200

    ex00 done

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bgrulois <bgrulois@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/03 10:33:50 by bgrulois          #+#    #+#             */
+/*   Updated: 2023/04/03 10:44:19 by bgrulois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int	is_empty_str(char *str)
@@ -7,6 +19,18 @@ int	is_empty_str(char *str)
 	while (str[++i])
 	{
 		if (str[i] != ' ')
+			return (0);
+	}
+	return (1);
+}
+
+int	all_strings_empty(char **av)
+{
+	int	i = 0;
+
+	while (av[++i])
+	{
+		if (is_empty_str(av[i]) == 0)
 			return (0);
 	}
 	return (1);
@@ -24,9 +48,9 @@ void	toupper_str(char *str)
 int	main(int ac, char **av)
 {
 	int	i = 0;
-	char	std_msg[21] = "LOUD NOISES WHATEVER";
+	char	std_msg[39] = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
-	if (ac == 1)
+	if (ac == 1 || all_strings_empty(av) == 1)
 		std::cout << std_msg;
 	else
 	{
@@ -37,9 +61,7 @@ int	main(int ac, char **av)
 				toupper_str(av[i]);
 				std::cout << av[i];
 			}
-			else
-				std::cout << std_msg;
-			if (av[i + 1])
+			if (av[i + 1] && !is_empty_str(av[i]))
 				std::cout << " ";
 		}
 	}
