ddd5e1ea21b62a635f7c63aff352f23f10e34313
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-05-09 04:50:53 -0400

    00-01 format update

--- 00/ex01/Contact.hpp
+++ 00/ex01/Contact.hpp
@@ -18,18 +18,17 @@
 
 class	Contact
 {
-public:
+	public:
 
-Contact( void );
+		Contact( void );
+		~Contact( void );
 
-	void		display_contact_info();
-	std::string	first_name;
-	std::string	last_name;
-	std::string	nickname;
-	std::string	phone_number;
-	std::string	darkest_secret;
-
-~Contact( void );
+		void		display_contact_info();
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secret;
 };
 
 #endif
--- 00/ex01/contact.cpp
+++ 00/ex01/contact.cpp
@@ -18,11 +18,11 @@ Contact::Contact( void ) {
 
 void	Contact::display_contact_info()
 {
-	std::cout << "First name : " << this->first_name << std::endl;
-	std::cout << "Last name : " << this->last_name << std::endl;
-	std::cout << "Nickname : " << this->nickname << std::endl;
-	std::cout << "Le 06: " << this->phone_number << std::endl;
-	std::cout << "Darkest Secret: " << this->darkest_secret << std::endl;
+	std::cout << "First name	:  " << this->first_name << std::endl;
+	std::cout << "Last name	:  " << this->last_name << std::endl;
+	std::cout << "Nickname	:  " << this->nickname << std::endl;
+	std::cout << "Phone number	:  " << this->phone_number << std::endl;
+	std::cout << "Darkest secret	:  " << this->darkest_secret << std::endl;
 }
 
 Contact::~Contact( void ) {
