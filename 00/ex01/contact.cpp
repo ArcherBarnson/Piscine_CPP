69bd1e2efbe22abffe8ce95bee5a41eaacf43015
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-06-29 17:28:11 -0400

    update 00ex01

--- 00/ex01/contact.cpp
+++ 00/ex01/contact.cpp
@@ -16,13 +16,73 @@ Contact::Contact( void ) {
 	return ;
 }
 
+void	Contact::set_contact_info(int mode, std::string &str)
+{
+	if (mode == 0)
+		this->_first_name = str;
+	if (mode == 1)
+		this->_last_name = str;
+	if (mode == 2)
+		this->_nickname = str;
+	if (mode == 3)
+		this->_phone_number = str;
+	if (mode == 4)
+		this->_darkest_secret = str;
+}
+
+void	Contact::fill_contact_infos()
+{
+	std::string info;
+
+	std::cout << "First Name : ";
+	std::getline(std::cin, info);
+	if (std::cin.eof())
+		return ;
+	this->set_contact_info(0, info);
+	std::cout << "Last Name : ";
+	std::getline(std::cin, info);
+	if (std::cin.eof())
+		return ;
+	this->set_contact_info(1, info);
+	std::cout << "Nickname : ";
+	std::getline(std::cin, info);
+	if (std::cin.eof())
+		return ;
+	this->set_contact_info(2, info);
+	std::cout << "Phone Number: ";
+	std::getline(std::cin, info);
+	if (std::cin.eof())
+		return ;
+	this->set_contact_info(3, info);
+	std::cout << "Darkest Secret : ";
+	std::getline(std::cin, info);
+	if (std::cin.eof())
+		return ;
+	this->set_contact_info(4, info);
+}
+
+std::string	Contact::get_contact_info(int mode)
+{
+	if (mode == 0)
+		return (this->_first_name);
+	if (mode == 1)
+		return (this->_last_name);
+	if (mode == 2)
+		return (this->_nickname);
+	if (mode == 3)
+		return (this->_phone_number);
+	if (mode == 4)
+		return (this->_darkest_secret);
+	return (NULL);
+}
+
 void	Contact::display_contact_info()
 {
-	std::cout << "First name	:  " << this->first_name << std::endl;
-	std::cout << "Last name	:  " << this->last_name << std::endl;
-	std::cout << "Nickname	:  " << this->nickname << std::endl;
-	std::cout << "Phone number	:  " << this->phone_number << std::endl;
-	std::cout << "Darkest secret	:  " << this->darkest_secret << std::endl;
+	std::cout << "First name	:  " << this->get_contact_info(0) << std::endl;
+	std::cout << "Last name	:  " << this->get_contact_info(1) << std::endl;
+	std::cout << "Nickname	:  " << this->get_contact_info(2) << std::endl;
+	std::cout << "Phone number	:  " << this->get_contact_info(3) << std::endl;
+	std::cout << "Darkest secret	:  " << this->get_contact_info(4) << std::endl;
 }
 
 Contact::~Contact( void ) {
