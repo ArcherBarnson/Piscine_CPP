0dbf35f9735dc3b2dab9918b2e692dec6cd2f4aa
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-06-30 12:41:52 -0400

    update initial amount

--- 00/ex02/Account.cpp
+++ 00/ex02/Account.cpp
@@ -10,6 +10,7 @@ int	Account::_totalNbWithdrawals = 0;
 Account::Account( int initial_deposit)
 	: _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
 	_nbAccounts++;
+	_totalAmount += initial_deposit;
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
