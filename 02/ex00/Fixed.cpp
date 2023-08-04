aa2325b335b8f10b2d4783dd0900e312021d0ea1
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-08-04 15:37:00 -0400

    02ex00 ok

--- 02/ex00/Fixed.cpp
+++ 02/ex00/Fixed.cpp
@@ -1,27 +1,33 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed( void ) : _fixed(0) {
+	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
-Fixed::Fixed(Fixed const & copy) {
+Fixed::Fixed(Fixed const &src) {
 	std::cout << "Copy contructor called" << std::endl;
+	_fixed = src.getRawBits();
 	return ;
 }
 
-Fixed::Fixed& operator=(const Fixed& =) {
+Fixed & Fixed::operator=( const Fixed & other) {
 	std::cout << "Assignation operator called" << std::endl;
+	this->_fixed = other.getRawBits();
 	return *this;
 }
 
 Fixed::~Fixed( void ) {
+	std::cout << "Destructor called" << std::endl;
 	return;
 }
 
-Fixed::int	getRawBits( void ) const {
-	return _raw;
+int	Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return _fixed;
 }
 
-Fixed::void	setRawBits(int const raw) {
-	_raw = raw;
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	_fixed = raw;
 }
--- 02/ex00/Fixed.hpp
+++ 02/ex00/Fixed.hpp
@@ -6,15 +6,15 @@
 class	Fixed {
 	public:
 		Fixed( void );
-		Fixed(Fixed const & copy);
-		Fixed& operator=(Fixed const & =);
+		Fixed(Fixed const &copy);
+		Fixed& operator=(const Fixed & other);
 		~Fixed( void );
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
 	
 	private:
 		int			_fixed;
-		static int const 	&_raw;
+		static int const 	_raw = 8;
 };
 
 #endif
